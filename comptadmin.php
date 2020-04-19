f302163bbe36a57952edce4be5a9b9f8cd2a3aa6
Doro402 <63165530+Doro402@users.noreply.github.com>
2020-04-19 20:53:09 +0200

    Doro

---
+++ comptadmin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Mini Projet de PHP</title>
+    <link rel="stylesheet" href="comptadmin.css">
+    <meta content="text/html; charset=utf-8"/>
+    <link rel="stylesheet" href="css/styleAdminQuestion.css">
+  </head>
+  <body>
+      <div>
+    <p class="message"> Le plaisir de jouer </p>  
+    <div class="quiz"><h2 class="h2">Creer et parametrer votre quizz</h2>
+<h3 class="deconexion"> Déconnexion</h3>        
+</div>
+<h2 class="prametr">PaRAMETREr VOTRE QUESTION</h2>
